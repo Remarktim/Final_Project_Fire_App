a34f953c99c5a63876c926c61de0c754392bae9f
Remarktim <remarktimones415@gmail.com>
2024-05-31 13:29:46 +0800

    Integrated Chart.js with dynamic data

--- projectsite/fire/views.py
+++ projectsite/fire/views.py
@@ -1,5 +1,10 @@
+from django.db import connection
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.db.models.functions import ExtractMonth
+from django.db.models import Count
+from datetime import datetime
 from fire.models import Locations, Incident
 
 class HomePageView(ListView):
@@ -14,6 +19,37 @@ class ChartView(ListView):
         context = super().get_context_data(**kwargs)
         return context
 
-    def get_queryset(self, *args, **kwargs):
-        pass
+def PieCountbySeverity(request):
+    query = '''
+    SELECT severity_level, COUNT(*) as count
+    FROM fire_incident
+    GROUP BY severity_level
+    '''
+    data = {}
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
 
+    if rows:
+        data = {severity: count for severity, count in rows}
+    else:
+        data = {}
+
+    return JsonResponse(data)
+
+def LineCountbyMonth(request):
+    current_year = datetime.now().year
+    result = {month: 0 for month in range(1, 13)}
+
+    incidents_per_month = Incident.objects.filter(date_time__year=current_year).values_list('date_time', flat=True)
+
+    for date_time in incidents_per_month:
+        month = date_time.month
+        result[month] += 1
+
+    month_names = {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun',
+                   7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'}
+
+    result_with_month_names = {month_names[int(month)]: count for month, count in result.items()}
+
+    return JsonResponse(result_with_month_names)
--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,11 +1,12 @@
+# urls.py
 from django.contrib import admin
 from django.urls import path
-from fire.views import HomePageView, ChartView
-from fire import views
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
-    path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
-
+    path('dashboard_chart/', ChartView.as_view(), name='dashboard-chart'),
+    path('chart/', PieCountbySeverity, name='chart'),
+    path('lineChart/', LineCountbyMonth, name='line-chart'),
 ]
